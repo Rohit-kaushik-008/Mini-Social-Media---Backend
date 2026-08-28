1b46dc1e3ad970774a249fdc0664d431c82985d2
Rohit Kaushik <rohitkaushik12801@gmail.com>
2026-08-28 14:31:31 +0530

    feature Done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,6 +2,7 @@ import { Post } from "../models/post.model.js";
 import uploadOnCloudinary from "../services/Cloudinary.js";
 import { responseHandler, errorHandler } from "../utils/responseHandler.js";
 
+// Create Post Controller
 export const createPost = async (req, res) => {
   try {
     const { caption } = req.body;
@@ -44,6 +45,104 @@ export const createPost = async (req, res) => {
       statusCode: 500,
       message: "something went wrong!",
       error: error.message,
+    });
+  }
+};
+
+// Get all Post Controller
+export const getAllPosts = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const posts = await Post.find({
+      author: userId,
+    });
+    return responseHandler({
+      res,
+      statusCode: 200,
+      message: "All Posts Fetched Successfully",
+      data: posts,
+    });
+  } catch (error) {
+    return errorHandler({
+      res,
+      statusCode: 500,
+      message: "Something went wrong!",
+      error: error.message,
+    });
+  }
+};
+
+// Delete Post Controller
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.userId;
+
+    const post = await Post.findOneAndDelete({
+      _id: postId,
+      author: userId,
+    });
+
+    if (!post) {
+      return responseHandler({
+        res,
+        statusCode: 404,
+        message: "Post not found or you are not authorized to delete it",
+      });
+    }
+
+    return responseHandler({
+      res,
+      statusCode: 200,
+      message: "Post Deleted Successfully",
+      data: post,
+    });
+  } catch (error) {
+    return errorHandler({
+      res,
+      statusCode: 500,
+      message: "Something went wrong!",
+      error: error.message,
+    });
+  }
+};
+
+// Update Post Controller
+export const updatePost = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const postId = req.params.id;
+    const { caption } = req.body;
+
+    const post = await Post.findOneAndUpdate(
+      { _id: postId, author: userId },
+      { caption },
+      {
+        returnDocument: "after",
+      },
+    );
+
+    if (!post) {
+      return responseHandler({
+        res,
+        statusCode: 404,
+        message: "Post not found or you are not authorized to update it",
+      });
+    }
+
+    return responseHandler({
+      res,
+      statusCode: 200,
+      message: "Post Updated Successfully",
+      data: post,
+    });
+  } catch (error) {
+    return errorHandler({
+      res,
+      statusCode: 500,
+      message: "Something went wrong",
+      errro: error.message,
     });
   }
 };
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getAllPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/feed", authMiddleware);
+router.get("/feed", authMiddleware, getAllPosts);
 router.post("/addPost", authMiddleware, upload.single("image"), createPost);
-router.patch("/editPost/:id", authMiddleware);
-router.delete("/deletePost/:id", authMiddleware);
+router.patch("/editPost/:id", authMiddleware, updatePost);
+router.delete("/deletePost/:id", authMiddleware, deletePost);
 
 export default router;
